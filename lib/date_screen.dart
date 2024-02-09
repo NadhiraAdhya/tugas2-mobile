6cb077b85f810943f6cc62d6b37cd70ede42606f
NadhiraAdhya <149305872+NadhiraAdhya@users.noreply.github.com>
2024-02-09 22:16:15 +0700

    date_screen.dart

--- lib/date_screen.dart
+++ lib/date_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class DateScreen extends StatefulWidget {
   const DateScreen({Key? key}) : super(key: key);
@@ -10,106 +8,135 @@ class DateScreen extends StatefulWidget {
 }
 
 class _DateScreenState extends State<DateScreen> {
-  final DateFormat _dateFormat = DateFormat('dd-MM-yyyy');
-  late DateTime _selectedDate = DateTime.now();
+  int _questionIndex = 0;
+  int _score = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _selectedDate = DateTime.now();
+  final List<Question> _questions = [
+    Question(
+      imageUrl: 'https://www.shutterstock.com/image-vector/wheel-trencher-construction-machine-vector-260nw-2137428955.jpg',
+      options: ['Truck', 'Bus', 'Trencher', 'Excavator'],
+      correctOptionIndex: 1,
+    ),
+    Question(
+      imageUrl: 'https://img2.pngdownload.id/20180209/rce/kisspng-loader-heavy-equipment-excavator-clip-art-hand-painted-bulldozers-5a7dba328e3f44.6966737315181891065827.jpg',
+      options: ['Grader', 'Buldozzer', 'Dump Truck', 'Paver'],
+      correctOptionIndex: 2,
+    ),
+    Question(
+      imageUrl: 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/13435/forklift-clipart-md.png',
+      options: ['Forklift', 'Crane', 'Compactor', 'Loader'],
+      correctOptionIndex: 3,
+    ),
+    Question(
+      imageUrl: 'https://clipart-library.com/newhp/19-194952_png-library-library-excavator-clipart-transparent-excavator-clipart.png',
+      options: ['Trencher', 'Paver', 'Truck', 'Excavator'],
+      correctOptionIndex: 4,
+    ),
+    Question(
+      imageUrl: 'https://www.pngitem.com/pimgs/m/277-2779409_caterpillar-inc-bulldozer-clipart-hd-png-download.png',
+      options: ['Buldozzer', 'Excavator', 'Crane', 'Grader'],
+      correctOptionIndex: 5,
+    ),
+    // Tambahkan pertanyaan lebih banyak jika diperlukan
+  ];
+
+  void _answerQuestion(int selectedOptionIndex) {
+    if (_questions[_questionIndex].correctOptionIndex == selectedOptionIndex) {
+      // Jawaban benar
+      setState(() {
+        _score++;
+      });
+    }
+
+    // Pindah ke pertanyaan berikutnya
+    setState(() {
+      _questionIndex++;
+    });
+
+    // Jika sudah menjawab semua pertanyaan, tampilkan skor akhir
+    if (_questionIndex == _questions.length) {
+      _showResultDialog();
+    }
   }
 
-  void _showDateIOS(BuildContext context) {
-    showCupertinoModalPopup(
+  void _showResultDialog() {
+    showDialog(
       context: context,
-      builder: (BuildContext builder) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          color: Colors.white,
-          height: MediaQuery.of(context).copyWith().size.height * 0.35,
-          child: CupertinoDatePicker(
-            mode: CupertinoDatePickerMode.date,
-            onDateTimeChanged: (value) {
-              setState(() {
-                _selectedDate = value;
-              });
-            },
-            initialDateTime: _selectedDate,
-            minimumYear: 2000,
-            maximumYear: 3000,
-          ),
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Skor Akhir'),
+          content: Text('Skor Anda: $_score dari ${_questions.length}'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Tutup dialog
+                // Reset kuis
+                setState(() {
+                  _questionIndex = 0;
+                  _score = 0;
+                });
+              },
+              child: Text('Ulangi Kuis'),
+            ),
+          ],
         );
       },
     );
   }
 
-  Future<void> _showDateAndroid(BuildContext context) async {
-    final datePick = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: _selectedDate.subtract(const Duration(days: 30)),
-      lastDate: DateTime(_selectedDate.year + 1),
-    );
-
-    if (datePick != null) {
-      setState(() {
-        _selectedDate = datePick;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+        title: Text('Quiz Tebak Gambar'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Text(
-                'Tanggal Terpilih: ${_dateFormat.format(_selectedDate)}',
-                style: TextStyle(fontSize: 18.0, color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue, // Ganti warna latar belakang
-                onPrimary: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-              ),
-              onPressed: () => _showDateAndroid(context),
-              child: const Text('Pilih Tanggal untuk Android'),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // Ganti warna latar belakang
-                onPrimary: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-              ),
-              onPressed: () => _showDateIOS(context),
-              child: const Text('Pilih Tanggal untuk iOS'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://i.pinimg.com/originals/bb/d4/a4/bbd4a42c6ce568edc36934b0437d51c1.jpg'), // Ganti dengan URL gambar latar belakang
+            fit: BoxFit.cover,
+          ),
         ),
+        child: _questionIndex < _questions.length
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      _questions[_questionIndex].imageUrl,
+                      width: 150.0, // Ganti dengan lebar yang diinginkan
+                      height: 150.0, // Ganti dengan tinggi yang diinginkan
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 20.0),
+                    ..._questions[_questionIndex].options.asMap().entries.map(
+                          (entry) => ElevatedButton(
+                            onPressed: () => _answerQuestion(entry.key),
+                            child: Text(entry.value),
+                          ),
+                        ),
+                  ],
+                ),
+              )
+            : Center(
+                child: ElevatedButton(
+                  onPressed: _showResultDialog,
+                  child: Text('Lihat Skor Akhir'),
+                ),
+              ),
       ),
     );
   }
+}
+
+class Question {
+  final String imageUrl;
+  final List<String> options;
+  final int correctOptionIndex;
+
+  Question({
+    required this.imageUrl,
+    required this.options,
+    required this.correctOptionIndex,
+  });
 }
