49ce96e5299e799a7421b39c517c6fb576c5368b
NadhiraAdhya <149305872+NadhiraAdhya@users.noreply.github.com>
2024-02-09 22:18:31 +0700

    Update mainscreen.dart

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:uts_mobile2_nadh/date_screen.dart';
-import 'package:uts_mobile2_nadh/list_screen.dart';
+import 'package:tubes_mobile2_nadh/date_screen.dart';
+import 'package:tubes_mobile2_nadh/list_screen.dart';
+import 'package:tubes_mobile2_nadh/pages/list_alat.dart';
+import 'package:tubes_mobile2_nadh/home_screen.dart';
+import 'package:tubes_mobile2_nadh/book_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -13,22 +16,30 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _index = 0;
   final List<Widget> _screens = [
-    Container(color: Colors.blueGrey),
-    Container(color: Colors.indigo),
+    const HomeScreen(),
+    const BookScreen(),
     const DateScreen(),
-    Container(color: Colors.teal),
+    const ListAlat(),
     const ListScreen(),
+    
+    Container(color: Colors.indigo),
+    const BookScreen(),
+    const DateScreen(),
+    const ListAlat(),
+    const ListScreen(),
+    
   ];
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Cari Alat Berat',
+          'Alat Berat',
           style: TextStyle(color: Colors.black),
         ),
-        backgroundColor: const Color.fromARGB(255, 64, 217, 255),
+        backgroundColor: Color.fromARGB(255, 61, 160, 218),
         elevation: 0,
         centerTitle: true,
       ),
@@ -38,13 +49,13 @@ class _MainScreenState extends State<MainScreen> {
         height: 60,
         items: <Widget>[
           Icon(Icons.home, size: 30, color: Colors.white),
-          Icon(Icons.search, size: 30, color: Colors.white),
-          Icon(Icons.calendar_today, size: 30, color: Colors.white),
+          Icon(Icons.book, size: 30, color: Colors.white),
+          Icon(Icons.quiz, size: 30, color: Colors.white),
           Icon(Icons.star, size: 30, color: Colors.white),
-          Icon(Icons.person, size: 30, color: Colors.white),
+          Icon(Icons.search, size: 30, color: Colors.white),
         ],
-        color: Colors.blueAccent,
-        buttonBackgroundColor: Colors.deepPurpleAccent,
+        color: Color.fromARGB(255, 68, 171, 255),
+        buttonBackgroundColor: const Color.fromARGB(255, 255, 77, 208),
         backgroundColor: Colors.transparent,
         animationCurve: Curves.easeInOut,
         animationDuration: const Duration(milliseconds: 600),
